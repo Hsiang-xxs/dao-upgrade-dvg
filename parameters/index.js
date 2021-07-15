a4ff4fec3f26935ef980699eefdbe5db37f3e9e6
Soon Lai <soonlai814@gmail.com>
2021-07-16 05:11:19 +0800

    Update global addresses

--- parameters/index.js
+++ parameters/index.js
@@ -9,20 +9,20 @@ module.exports = {
     },
     DVD: {
       tokenAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",  // TODO: Update after DVD deployed on Mainnet
-      vaultAddress: "0x0316e3f6e0a4c01ee54e27cb1c8b2f7655d4cb62",  // TODO: Update after DVD deployed on Mainnet
+      vaultAddress: "0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE",  // TODO: Update after DVD deployed on Mainnet
     },
   },
   kovan: {
     Global: {
-      proxyAdmin: "0xeFd9928Aa5A192C0267CdAed43235006B7A28628",  // TODO: Update with the correct address
-      signerAddress: "0xeFd9928Aa5A192C0267CdAed43235006B7A28628",  // TODO: Update with the correct address
+      proxyAdmin: "0xA1b0176B24cFB9DB3AEe2EDf7a6DF129B69ED376",  // TODO: Update with the correct address
+      signerAddress: "0xA1b0176B24cFB9DB3AEe2EDf7a6DF129B69ED376",  // TODO: Update with the correct address
     },
     DVG: {
       tokenAddress: "0xea9726eFc9831EF0499fD4Db4Ab143F15a797673",
     },
     DVD: {
-      tokenAddress: "0x07de306FF27a2B630B1141956844eB1552B956B5",  // TODO: Update after DVD deployed on Kovan
-      vaultAddress: "0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE",  // TODO: Update after DVD deployed on Kovan
+      tokenAddress: "0x6639c554A299D58284e36663f609a7d94526fEC0",  // TODO: Update after DVD deployed on Kovan
+      vaultAddress: "0x46d5D81D9C855ed58f35447cD0c1Dd0e07e967D2",  // TODO: Update after DVD deployed on Kovan
     },
   },
 };
