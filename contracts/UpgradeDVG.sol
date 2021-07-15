4e8a1f8a6d5deb4cb899022ca3ba4070f16b46dd
lucky83149028 <zhengyun50511@gmail.com>
2021-07-15 23:25:37 +0800

    The contract updated to check the signature

--- contracts/UpgradeDVG.sol
+++ contracts/UpgradeDVG.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPLv3
 pragma solidity 0.7.6;
+pragma abicoder v2;
 
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
@@ -11,17 +13,12 @@ contract UpgradeDVG is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    struct Holder {
-        uint256 balance;
-        uint256 swapped;
-    }
-
     IERC20 public dvg;
     IERC20 public dvd;
     address public vault;
-    mapping(address => Holder) public dvgHolders;
-    uint256 public dvgHolderCount;
-    uint256 public totalBalance;
+    address public signer;
+
+    mapping(address => uint256) public swappedAmounts;
     uint256 public totalSwapped;
 
     event DvgUpgrade(address indexed user, uint256 dvdAmount);
@@ -33,13 +30,14 @@ contract UpgradeDVG is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         _;
     }
 
-    function initialize(address _dvg, address _dvd, address _vault) public initializer {
+    function initialize(address _dvg, address _dvd, address _vault, address _signer) public initializer {
         __Ownable_init();
         __ReentrancyGuard_init();
 
         dvg = IERC20(_dvg);
         dvd = IERC20(_dvd);
         vault = _vault;
+        signer = _signer;
     }
 
     receive() external payable {
@@ -47,45 +45,23 @@ contract UpgradeDVG is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     }
 
     /**
-     * @notice Add the white list.
-     * @param _addresses Addresses of DVG token holders
-     * @param _amounts Amounts which the holders are holding
-     */
-    function addWhiteList(address[] memory _addresses, uint256[] memory _amounts) external onlyOwner {
-        require(0 < _addresses.length, "No address input");
-        require(_addresses.length == _amounts.length, "Mismatch the parameters");
-
-        for (uint i = 0; i < _addresses.length; i ++) {
-            address user = _addresses[i];
-            uint256 amount = _amounts[i];
-            require(user != address(0), "The address is invalid");
-            require(0 < amount, "The amount is invalid");
-            require(dvgHolders[user].balance == 0, "This address already added");
-
-            dvgHolders[user] = Holder({
-                balance: amount,
-                swapped: 0
-            });
-            totalBalance = totalBalance.add(amount);
-        }
-
-        dvgHolderCount = dvgHolderCount.add(_addresses.length);
-    }
-
-    /**
      * @notice Swap the DVG token in the same amount of DVD token.
-     * @param _amount Amount to upgrade
+     * @param _amountToSwap Amount to upgrade
+     * @param _allowedAmount Amount allowed to be upgraded
+     * @param _signature signature to proof both the sender and the allowed amount
      */
-    function upgradeDVG(uint256 _amount) external onlyEOA nonReentrant returns(uint256 dvdAmount) {
-        require(0 < _amount, "The amount is invalid");
-        address sender = msg.sender;
-        Holder memory holder = dvgHolders[sender];
-        uint256 pending = holder.balance.sub(holder.swapped);
+    function upgradeDVG(uint256 _amountToSwap, uint256 _allowedAmount, bytes memory _signature) external onlyEOA nonReentrant returns(uint256 dvdAmount) {
+        address sender = _msgSender();
+        require(0 < _amountToSwap, "The amountToSwap is invalid");
+        require(_amountToSwap <= _allowedAmount, "The amountToSwap must be equal or less than allowedAmount");
+        require(isValidSignature(sender, _allowedAmount, _signature), "The specified amount is not allowed for the sender");
+
+        uint256 pending = _allowedAmount.sub(swappedAmounts[sender]);
         require(0 < pending, "Sender already upgraded token for the allowed amount");
 
-        dvdAmount = (_amount < pending) ? _amount : pending;
+        dvdAmount = (_amountToSwap < pending) ? _amountToSwap : pending;
 
-        dvgHolders[sender].swapped = holder.swapped.add(dvdAmount);
+        swappedAmounts[sender] = swappedAmounts[sender].add(dvdAmount);
         totalSwapped = totalSwapped.add(dvdAmount);
 
         dvg.safeTransferFrom(sender, address(this), dvdAmount);
@@ -95,23 +71,37 @@ contract UpgradeDVG is OwnableUpgradeable, ReentrancyGuardUpgradeable {
 
     /**
      * @notice Airdrop the DVD tokens to the specified addresses.
-     * @param _addresses Addresses of DVG token holders.
+     * @param _addresses Addresses to airdrop DVD token
+     * @param _allowedAmounts Amounts allowed to be upgraded
+     * @param _signatures signatures to proof both the senders and the allowed amounts
      */
-    function airdropDVD(address[] memory _addresses) external onlyOwner {
+    function airdropDVD(address[] memory _addresses, uint256[] memory _allowedAmounts, bytes[] memory _signatures) external onlyOwner {
         require(0 < _addresses.length, "No address input");
+        require(_addresses.length == _allowedAmounts.length, "Mismatch the parameters");
+        require(_addresses.length == _signatures.length, "Mismatch the parameters");
 
         for (uint i = 0; i < _addresses.length; i ++) {
             address user = _addresses[i];
-            Holder memory holder = dvgHolders[user];
-            uint256 pending = holder.balance.sub(holder.swapped);
+            uint256 allowedAmount = _allowedAmounts[i];
+            bytes memory signature = _signatures[i];
+            require(isValidSignature(user, allowedAmount, signature), "The specified amount is not allowed for the user");
+
+            uint256 pending = allowedAmount.sub(swappedAmounts[user]);
             if (0 < pending) {
+                swappedAmounts[user] = swappedAmounts[user].add(pending);
                 totalSwapped = totalSwapped.add(pending);
-                holder.swapped = holder.balance;
-                dvgHolders[user] = holder;
 
                 dvd.safeTransferFrom(vault, user, pending);
                 emit DvdAirdrop(user, pending);
             }
         }
+    }
+
+    function isValidSignature(address _user, uint256 _allowedAmount, bytes memory _signature) internal view returns (bool) {
+        bytes32 message = ECDSA.toEthSignedMessageHash(keccak256(abi.encodePacked(_user, _allowedAmount)));
+
+        // check that the signature is from admin signer.
+        address recoveredAddress = ECDSA.recover(message, _signature);
+        return (recoveredAddress == signer) ? true : false;
     }
 }
